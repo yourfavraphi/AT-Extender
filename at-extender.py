a9d379547862d9f9a6a3fc57431d9a3ce9cd0d0f
Dinobeiser <72189316+Dinobeiser@users.noreply.github.com>
2025-04-05 20:46:24 +0200

    Update at-extender.py

--- at-extender.py
+++ at-extender.py
@@ -18,7 +18,7 @@ LOGIN_URL = "https://login.alditalk-kundenbetreuung.de/signin/XUI/#login/"
 DASHBOARD_URL = "https://www.alditalk-kundenportal.de/portal/auth/buchungsuebersicht/"
 UBERSICHT_URL = "https://www.alditalk-kundenportal.de/portal/auth/uebersicht/"
 
-VERSION = "1.0.6"  # Deine aktuelle Version
+VERSION = "1.0.7"  # Deine aktuelle Version
 
 REMOTE_VERSION_URL = "https://raw.githubusercontent.com/Dinobeiser/AT-Extender/main/version.txt"  # Link zur Version
 REMOTE_SCRIPT_URL = "https://raw.githubusercontent.com/Dinobeiser/AT-Extender/main/at-extender.py"  # Link zum neuesten Skript
